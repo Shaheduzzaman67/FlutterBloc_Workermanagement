ec0d8f8e6ca6e6819e12ce37b3d3f2de0630c7a0
ShishirOve <arcbuildergroup@gmail.com>
2020-03-29 20:07:35 +0600

    worker data created

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Worker Management",
       theme: ThemeData(
-        primarySwatch: Colors.purple;
+        primarySwatch: Colors.purple,
       ),
       home: HomePage(),
     );
