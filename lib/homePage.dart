e21d184549ef418ec938a5580dd63d76541201a2
ShishirOve <arcbuildergroup@gmail.com>
2020-03-29 21:35:24 +0600

    home page design

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -11,9 +11,62 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final WorkerBloc _workerBloc = WorkerBloc();
 
+  @override
+  void dispose(){
+    _workerBloc.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Worker Management"),
+      ),
+      body: Container(
+        child: StreamBuilder<List<Worker>>(
+          stream: _workerBloc.workerListStream,
+          // ignore: missing_return
+          builder: (BuildContext context, AsyncSnapshot<List<Worker>> snapshot){
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+              itemBuilder: (context, index){
+                  return Card(
+                    elevation:  5.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(20.0),
+                          child: Text(
+                            "${snapshot.data[index].id}.",
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                "${snapshot.data[index].name}.",
+                                style: TextStyle(fontSize: 15.0),
+                              ),
+                              Text(
+                                "${snapshot.data[index].salary}.",
+                                style: TextStyle(fontSize: 12.0),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+              },
+            );
+          } ,
+        ),
+      ),
+    );
   }
 }
